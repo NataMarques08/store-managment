71102294a658e8574951d7851f9c9185f1d692e6
NataMarques08 <nmolinamarques@gmail.com>
2025-04-03 00:38:47 -0300

    Implementing HttpVerbs

--- store-management-customers-and-products/src/main/java/com/nata/store_management_customers_and_products/mapper/CustomerMapper.java
+++ store-management-customers-and-products/src/main/java/com/nata/store_management_customers_and_products/mapper/CustomerMapper.java
@@ -1,6 +1,7 @@
 package com.nata.store_management_customers_and_products.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.nata.store_management_customers_and_products.dto.CustomerDtos.CustomerDTO;
 import com.nata.store_management_customers_and_products.model.Customer;
@@ -8,8 +9,8 @@ import com.nata.store_management_customers_and_products.model.Customer;
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
-
-    CustomerDTO convertEntityToDTO(Customer customer);
-
+    @Mapping(target = "creditlimit", source = "creditlimit")
     Customer convertDTOToEntity(CustomerDTO customerDTO);
+    CustomerDTO convertEntityToDTO(Customer customer);
+    
 }
