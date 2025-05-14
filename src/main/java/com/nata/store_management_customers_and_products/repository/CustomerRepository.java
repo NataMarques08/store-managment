d4bb4aae449ac161af938c8a598ec7d2b981d44c
NataMarques08 <40342913+NataMarques08@users.noreply.github.com>
2025-05-13 22:44:38 -0300

    Delete src directory

--- src/main/java/com/nata/store_management_customers_and_products/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nata.store_management_customers_and_products.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nata.store_management_customers_and_products.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long>{
-
-}
