ef264b6d7180fe8b4574e15dc6ada7c82cfd9424
NataMarques08 <nmolinamarques@gmail.com>
2025-05-13 22:27:57 -0300

    Creating Get Verb

---
+++ src/main/java/com/nata/store_management_customers_and_products/controller/CustomerController.java
@@ -0,0 +1,32 @@
+package com.nata.store_management_customers_and_products.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.nata.store_management_customers_and_products.dto.CustomerDtos.CustomerDTO;
+import com.nata.store_management_customers_and_products.model.Customer;
+import com.nata.store_management_customers_and_products.service.CustomerService;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<CustomerDTO> createCustomer(@RequestBody @Valid CustomerDTO customerDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.saveCustomer(customerDTO));
+    }
+    
+
+
+
+}
---
+++ src/test/java/com/nata/store_management_customers_and_products/StoreManagementCustomersAndProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nata.store_management_customers_and_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementCustomersAndProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
